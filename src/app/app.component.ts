03e36bd7fdee8ded156e64ddd08c372784b77dc6
user <email@example.com>
2024-05-26 19:21:07 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular18test';
+  counter = 0
+
+  increment() {
+    this.counter = this.counter + 1;
+  }
+  decrement() {
+    this.counter = this.counter - 1;
+  }
+  reset() {
+    this.counter = 0
+  }
 }
